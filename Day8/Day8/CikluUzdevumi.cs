80fd3ebf9cee41ccae881544029db5d27caaee2d
Emils Lapins <emilslapins@gmail.com>
2018-07-04 19:03:19 +0300

    Task1

---
+++ Day8/Day8/CikluUzdevumi.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day8
+{
+    class CikluUzdevumi
+    {
+
+        // cik reizu cikls iet cauri ar console.Readline()
+        //Kludas pazinojums, ja skaitlis ir 0 vai negativs!
+        /* 
+         * *
+         * * *
+         * * * *
+         * utt
+         */
+        public void Zvaigznite()
+        {
+            Console.WriteLine("Ludzu ievadiet skaitli!");
+            int Sakums = Convert.ToInt16(Console.ReadLine());
+
+            string Zvaigznite = "*";
+
+            if (Sakums > 0)
+            {
+                for (int i = 1; i < Sakums + 1; i++)
+                {
+                    Console.WriteLine(Zvaigznite);
+
+                    Zvaigznite = Zvaigznite + "*";
+
+                }
+            } else
+            {
+                Console.WriteLine("Error!");
+            }
+
+        }
+      
+    }
+}
--- Day8/Day8/Program.cs
+++ Day8/Day8/Program.cs
@@ -10,6 +10,32 @@ namespace Day8
     {
         static void Main(string[] args)
         {
+            // switch un case izvelni!!!
+            Console.WriteLine("Izveleties uzdevumu- 1 vai 2 ");
+            string choice = Console.ReadLine();
+            int ch = Convert.ToInt16(choice);
+
+            CikluUzdevumi CikUzd = new CikluUzdevumi();
+
+
+            switch (ch)
+            {
+                case 1:
+                    CikUzd.Zvaigznite();
+
+                    break;
+                case 2:
+                    
+                    break;
+              
+
+                default:
+                    Console.WriteLine("Nepareiza ievade!");
+                    break;
+            }
+            Console.ReadLine();
+
+
         }
     }
 }
